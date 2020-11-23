6ce8947ba590f36443f9e89fc5f77bc696cb6da7
saraaldr <74928739+saraaldr@users.noreply.github.com>
2020-11-23 21:49:12 +0300

    Add files via upload

---
+++ calories.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class calories {
+public static Scanner sc;
+
+    public static void main(String[] args) {
+        sc = new Scanner(System.in);
+        System.out.println("TO CALCULATE THE CALORIES OF YOUR MEAL :\n");
+        System.out.println("CHOOSE THE TYBE OF FOOD :\n 1-CHIKEN.\n 2-RICE \n 3-BREAD. \n"); 
+   
+    }
+    
+}
